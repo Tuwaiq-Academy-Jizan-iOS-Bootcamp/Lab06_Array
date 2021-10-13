aa17802c454c2c79551270d79e9b260558df9448
Arwa Alattas <arwaalattas@MacBook-Pro-Arwa.local>
2021-10-13 23:58:26 +0300

    my homework of lab6

---
+++ lab6.playground/Contents.swift
@@ -0,0 +1,68 @@
+//the solution of first point the homework
+var reverseArray = [1,2,3,4,5,6,7]
+print(reverseArray)
+print("the array in reverse order ")
+for i in reverseArray.reversed()
+     {
+    print(i)
+}
+
+print("the array in random order ")
+for i in reverseArray.shuffled()
+     {
+    print(i)
+}
+
+//the solution of point 2 and 3 in the homework
+ 
+ var oddAndEvenNmber =
+[1,10,22,44,5,13,19,23,99,9]
+for i in oddAndEvenNmber{
+if (i % 2 == 0){
+    print("this is even number \(i)")
+}}
+for i in oddAndEvenNmber{
+if (i % 2 != 0){
+    
+    print("this is odd number \(i)")
+}}
+
+
+// the solution of 4 and 5 point in the Homework
+var maxAndMin = [1,10,22,44,5,13,19,23,99,9]
+print(maxAndMin)
+
+print("the maximum number is \(maxAndMin.max()!)")
+print("the minimum number is \(maxAndMin.min()!)")
+
+// the solution of 6 and 7 point in the Homework
+
+var array = [1,10,22,44,5,13,19,23,99,9]
+print (array)
+var sum = array.reduce(0,+)
+print ("the sum of element in array = \(sum)")
+var avg = sum / array.count
+print("the average of element in array = \(avg)")
+
+
+//the solution of point 8 in the Homewoork
+var arr = ["I love coding","swift is the best language","coding is fun!"]
+
+if  let longest = arr.max(by: {$1.count
+    > $0.count}){
+    print(longest)
+}
+
+//the solution of point 9 and 10 in Homework
+        
+var arrr = ["I love coding", "swift is the best language","coding is fun!"].map {$0.count}
+print(arrr)
+for i in arrr{
+if (i % 2 != 0){
+    print("the odd length \(i)")}
+}
+for i in arrr{
+if (i % 2 == 0){
+    print("the even length \(i)")}
+    
+}
