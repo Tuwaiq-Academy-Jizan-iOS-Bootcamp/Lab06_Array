a581d361e59dcce61a6e56181a0326d29dccd4cf
Ehab hakami <ehab7k@gmail.com>
2021-10-13 19:42:57 +0300

    ehablap6

---
+++ Lab6.playground/Contents.swift
@@ -0,0 +1,34 @@
+
+var arr = [1,10,22,44,5,13,19,23,99,9]
+var ars = ["I love coding", "swift is the best language","coding is fun!"]
+var avg = (arr.reduce(0, +) / arr.count)
+arr.reverse()
+print(arr)
+for we in ars{
+    if(we.count % 2 == 0){
+        print("\(we) is even")
+    }else{
+        print("\(we) is odd")
+    }
+}
+for q in arr{
+    if(q % 2 == 0){
+        print("\(q) is even")
+    }else{
+        print("\(q) is odd")
+    }
+
+
+print(arr.min()!)
+
+print(arr.max()!)
+
+print(arr.reduce(0, +))
+
+print(avg)
+print(ars[1])
+}
+
+
+
+
